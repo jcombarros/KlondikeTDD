1b37f44b8e7879d7ef3270d1344a54bc4b2dd015
Javier Combarros <Javier@138.4.192.251>
2015-04-15 22:05:41 +0200

    Added before() method to moveDeckWasteControllerTest

--- src/test/MoveDeckWasteControllerTest.java
+++ src/test/MoveDeckWasteControllerTest.java
@@ -3,15 +3,16 @@ package test;
 import static org.junit.Assert.*;
 import klondike.controllers.MoveDeckWasteController;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MoveDeckWasteControllerTest {
 	
 	private MoveDeckWasteController moveDeckWasteController;
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	@Before
+	public void before(){
+		moveDeckWasteController = new MoveDeckWasteController();
 	}
 
 }

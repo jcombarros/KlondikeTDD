26ad20166caddf671ddaabeebfdc77fdba9792f1
Javier Combarros <Javier@138.4.192.251>
2015-04-16 21:33:50 +0200

    Added before() method to MoveWasteTableauControllerTest

--- src/test/MoveWasteTableauControllerTest.java
+++ src/test/MoveWasteTableauControllerTest.java
@@ -1,9 +1,16 @@
 package test;
 
+import org.junit.Before;
+
 import klondike.controllers.MoveWasteTableauController;
 
 public class MoveWasteTableauControllerTest {
 	
 	private MoveWasteTableauController moveWasteTableauController;
+	
+	@Before
+	public void before(){
+		moveWasteTableauController = new MoveWasteTableauController();
+	}
 
 }

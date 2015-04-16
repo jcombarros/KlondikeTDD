3bb4190367822e4cd6ef8e5b04e660d366c8abd8
Javier Combarros <Javier@138.4.192.251>
2015-04-16 21:32:54 +0200

    Added MoveWasteTableauController class

--- src/test/MoveWasteTableauControllerTest.java
+++ src/test/MoveWasteTableauControllerTest.java
@@ -1,5 +1,9 @@
 package test;
 
+import klondike.controllers.MoveWasteTableauController;
+
 public class MoveWasteTableauControllerTest {
+	
+	private MoveWasteTableauController moveWasteTableauController;
 
 }

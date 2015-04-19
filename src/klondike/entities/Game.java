31319e96cafd987223eb1d59f8ab9f9b5dc8d15e
Javier Combarros <Javier@138.4.192.251>
2015-04-19 19:14:52 +0200

    Added Stack of Cards properties in Game class for Waste Deck Tableaus and Foundations

--- src/klondike/entities/Game.java
+++ src/klondike/entities/Game.java
@@ -1,10 +1,53 @@
 package klondike.entities;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Game {
 	
 	public static int NUM_FOUNDATIONS = 4;
 	
 	public static int NUM_TABLEAUS = 7;
+	
+	private Stack<Card> waste;
+	
+	private Stack<Card> deck;
+	
+	private ArrayList<Stack<Card>> tableaus;
+	
+	private ArrayList<Stack<Card>> foundations;
+
+	public Stack<Card> getWaste() {
+		return waste;
+	}
+
+	public void setWaste(Stack<Card> waste) {
+		this.waste = waste;
+	}
+
+	public Stack<Card> getDeck() {
+		return deck;
+	}
+
+	public void setDeck(Stack<Card> deck) {
+		this.deck = deck;
+	}
+
+	public ArrayList<Stack<Card>> getTableaus() {
+		return tableaus;
+	}
+
+	public void setTableaus(ArrayList<Stack<Card>> tableaus) {
+		this.tableaus = tableaus;
+	}
+
+	public ArrayList<Stack<Card>> getFoundations() {
+		return foundations;
+	}
+
+	public void setFoundations(ArrayList<Stack<Card>> foundations) {
+		this.foundations = foundations;
+	}
 
 	public boolean isFinished() {
 		// TODO Auto-generated method stub

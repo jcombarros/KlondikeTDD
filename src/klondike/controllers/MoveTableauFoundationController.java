1dd858406783c000ae2d27942e0c2bd64b0c9920
Javier Combarros <Javier@138.4.192.251>
2015-04-19 20:30:35 +0200

    Added before() method to MoveTableauFoundationControllerTest

--- src/klondike/controllers/MoveTableauFoundationController.java
+++ src/klondike/controllers/MoveTableauFoundationController.java
@@ -1,5 +1,13 @@
 package klondike.controllers;
 
+import klondike.entities.Game;
+
 public class MoveTableauFoundationController {
 
+private Game game;
+	
+	public MoveTableauFoundationController(Game game){
+		this.game = game;
+	}
+	
 }
--- src/test/MoveTableauFoundationControllerTest.java
+++ src/test/MoveTableauFoundationControllerTest.java
@@ -1,10 +1,18 @@
 package test;
 
+import org.junit.Before;
+
 import klondike.controllers.MoveTableauFoundationController;
+import klondike.entities.Game;
 
 public class MoveTableauFoundationControllerTest {
 	
+	private Game game = new Game();
 	private MoveTableauFoundationController moveTableauFoundationController;
 
+	@Before
+	public void before(){
+		moveTableauFoundationController = new MoveTableauFoundationController(game);
+	}
 
 }
